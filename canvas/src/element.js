8c8b9efcc52044d1dc064a595d1c27403c4a0f13
Gianluca Casati <fibo@users.noreply.github.com>
2025-08-30 11:17:20 +0700

    can pick cells

--- canvas/src/element.js
+++ canvas/src/element.js
@@ -1,15 +1,19 @@
-import { AmbientLight, Group, Mesh, MeshBasicMaterial, PerspectiveCamera, Scene, SphereGeometry, WebGLRenderer } from 'three'
+import { AmbientLight, Group, Mesh, MeshBasicMaterial, PerspectiveCamera, Raycaster, Scene, SphereGeometry, Vector3, WebGLRenderer } from 'three'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 import { Tris3dBoard } from '@tris3d/game'
 
 class Tris3dCanvas extends HTMLElement {
   board = new Tris3dBoard()
+  canvas = document.createElement('canvas')
+
   group = new Group()
   scene = new Scene()
   ambientLight = new AmbientLight(0x404040)
+  ray = new Raycaster()
+
+  cells = []
 
   size = 500
-  gotControlsChange = true
   shouldRotateGroup = true
 
   connectedCallback() {
@@ -19,12 +23,32 @@ class Tris3dCanvas extends HTMLElement {
     this.setupControls()
     this.setupGeometry()
     this.setupLights()
+    this.addEventListeners()
     this.mainLoop()
   }
 
+  handleEvent(event) {
+    if (event.type === 'pointerdown') {
+      this.shouldRotateGroup = false
+      const cell = this.pickCell(event)
+      if (cell) {
+        cell.material.transparent = false
+      }
+    }
+  }
+
+  addEventListeners() {
+    const { canvas } = this
+    canvas.addEventListener('pointerdown', this)
+  }
+
+  removeEventListeners() {
+    const { canvas } = this
+    canvas.removeEventListener('pointerdown', this)
+  }
+
   createRenderer() {
-    const { size } = this
-    const canvas = document.createElement('canvas')
+    const { canvas, size } = this
     const renderer = this.renderer = new WebGLRenderer({ canvas, alpha: true, antialias: true })
     renderer.setSize(size, size)
     renderer.setPixelRatio(window.devicePixelRatio)
@@ -34,14 +58,9 @@ class Tris3dCanvas extends HTMLElement {
 
   mainLoop() {
     const next = () => {
-      let shoudRender = false
+      let shoudRender = true
       if (this.shouldRotateGroup) {
-        shoudRender = true
         this.group.rotation.y += 0.005
-      }
-      if (this.gotControlsChange) {
-        shoudRender = true
-        this.gotControlsChange = false
       }
       if (shoudRender) {
         this.renderer.render(this.scene, this.camera)
@@ -49,6 +68,21 @@ class Tris3dCanvas extends HTMLElement {
       requestAnimationFrame(next)
     }
     next() // Oh yeah!
+  }
+
+  pickCell(event) {
+    const { canvas, camera, cells, ray } = this
+    const rect = canvas.getBoundingClientRect()
+    const x = ((event.clientX - rect.left) / rect.width) * 2 - 1
+    const y = -((event.clientY - rect.top) / rect.height) * 2 + 1
+    const pointerVector = new Vector3(x, y, 1)
+    pointerVector.unproject(camera)
+    ray.set(camera.position, pointerVector.sub(camera.position).normalize())
+    const intersects = ray.intersectObjects(cells)
+    const firstMatch = intersects[0]
+    if (firstMatch) {
+      return firstMatch.object
+    }
   }
 
   setStyle() {
@@ -76,25 +110,24 @@ class Tris3dCanvas extends HTMLElement {
     controls.update()
     controls.addEventListener('change', () => {
       this.shouldRotateGroup = false
-      this.gotControlsChange = true
-      this.shouldAnimate = true
     })
   }
 
   setupGeometry() {
     const { group, scene } = this
-    const material = new MeshBasicMaterial({
-      color: 0x333333,
-      transparent: true,
-      opacity: 0.1,
-    })
     for (let x = -2; x <= 2; x += 2)
       for (let y = -2; y <= 2; y += 2)
         for (let z = -2; z <= 2; z += 2) {
           const geometry = new SphereGeometry(1)
+          const material = new MeshBasicMaterial({
+            color: 0x333333,
+            transparent: true,
+            opacity: 0.1,
+          })
           const sphere = new Mesh(geometry, material)
           sphere.position.set(x, y, z)
           group.add(sphere)
+          this.cells.push(sphere)
         }
     scene.add(group)
   }
